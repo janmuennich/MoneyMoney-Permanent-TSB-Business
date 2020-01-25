90ab4182621bf57dcb6193fff63862eb2053be50
Jan Münnich <jan@dotplex.com>
2020-01-25 10:45:18 +0000

    Harmonise tab stops

--- Permanent TSB Business.lua
+++ Permanent TSB Business.lua
@@ -6,7 +6,7 @@ WebBanking{version     = 1.00,
            description = string.format(MM.localizeText("Get balance and transactions for %s"), BANK_CODE)}
 
 function SupportsBank (protocol, bankCode)
-	return protocol == ProtocolWebBanking and bankCode == BANK_CODE
+    return protocol == ProtocolWebBanking and bankCode == BANK_CODE
 end
 
 local connection = nil
@@ -14,8 +14,8 @@ local logoutUrl = nil
 local startPage = nil
 
 function InitializeSession (protocol, bankCode, username, username2, password, username3)
-	-- Split password and PAN
-	local index, _, pass, pan = string.find(password, "^(.+)%-([%d]+)$")
+    -- Split password and PAN
+    local index, _, pass, pan = string.find(password, "^(.+)%-([%d]+)$")
     if index ~= 1 then
         return "Please enter your password followed by a minus sign and your six-digit PAN. (example: password-123456)"
     end
@@ -66,22 +66,22 @@ function InitializeSession (protocol, bankCode, username, username2, password, u
 end
 
 function ListAccounts(knownAccounts)
-	-- Return array of accounts.
+    -- Return array of accounts.
     local accounts = {}    
 
     startPage:xpath("//div[contains(@class, 'module-account')]"):each(
         function(index, element)
-        	local count, _, accountNumber = string.find(element:xpath(".//div[@class='col5']/p/span"):text(), "xxxx ([%d]+)")
+            local count, _, accountNumber = string.find(element:xpath(".//div[@class='col5']/p/span"):text(), "xxxx ([%d]+)")
             table.insert(accounts, {
-            	type = AccountTypeGiro,
-            	currency = "EUR",
+                type = AccountTypeGiro,
+                currency = "EUR",
                 name = element:xpath(".//h2/a"):text(),
                 accountNumber = accountNumber
             })
         end
     )
 
-	return accounts
+    return accounts
 end
 
 function RefreshAccount(account, since)
@@ -101,11 +101,11 @@ function RefreshAccount(account, since)
                 balance = tonumber(balanceStr)
                 
                 if since < (os.time() - 2592000) then
-                	months = 3
+                    months = 3
                 end
                 
                 local count, _, accountId = string.find(element:xpath(".//h2/a"):attr("href"), ".+accountId=(.+)$")
-				statementUrl = '/online/Accounts/Details/RecentTransactions?accountId=' .. accountId .. "&months=" .. months
+                statementUrl = '/online/Accounts/Details/RecentTransactions?accountId=' .. accountId .. "&months=" .. months
                 statementPage = HTML(connection:request("GET", statementUrl))
              end
         end
@@ -118,79 +118,79 @@ function RefreshAccount(account, since)
     local transactions = {}
 
     -- load transactions
-	local transactionDetails = statementPage:xpath("//div[contains(@class, 'module-account-detail')]/*/*/tbody")
+    local transactionDetails = statementPage:xpath("//div[contains(@class, 'module-account-detail')]/*/*/tbody")
 
-	transactionDetails:children():each(
-		function(index, element)
-			local bookingText = nil
-			local purpose = nil
-			local endToEndReference = nil
-		
-			local firstElement = element:children():get(1)
-			local timestamp = humanDateStrToTimestamp(firstElement:text())
+    transactionDetails:children():each(
+        function(index, element)
+            local bookingText = nil
+            local purpose = nil
+            local endToEndReference = nil
+        
+            local firstElement = element:children():get(1)
+            local timestamp = humanDateStrToTimestamp(firstElement:text())
 
-			local descriptionElement = element:children():get(2)
-			local description = descriptionElement:text()
-			
-			if (string.sub(description, 0, 2)) == 'CT' then
-				bookingText = 'Credit Transfer'
-				description = string.sub(description, 3, -1)
-			elseif (string.sub(description, 0, 2)) == 'DD' then
-				bookingText = 'Direct Debit'
-				description = string.sub(description, 3, -1)
-			elseif (string.sub(description, 0, 3)) == 'POS' then
-				bookingText = 'Debit Card Transaction'
-				description = string.sub(description, 4, -1)
-			elseif (string.sub(description, 0, 3)) == 'CNC' then
-				bookingText = 'Debit Card Transaction (Contactless)'
-				description = string.sub(description, 4, -1)
-			elseif (string.sub(description, 0, 3)) == 'ATM' then
-				bookingText = 'ATM Transaction'
-				description = string.sub(description, 4, -1)
-			end
-			
-			-- Get SEPA details
-			local sepaForm = descriptionElement:xpath(".//form")
-			if sepaForm:length() > 0 then
-				local sepaPage = HTML(connection:request(sepaForm:submit()))
-				if sepaPage == nil then
-			        error("Could not retrieve SEPA details for " .. firstElement:text() .. " " .. description)
-			    end
-   				local sepaVars = sepaPage:xpath("//div[@class='value']")
-				description = sepaVars:get(1):text()
-				purpose = sepaVars:get(3):text() .. " " .. sepaVars:get(9):text()
-				endToEndReference = sepaVars:get(8):text()
-			end
-			
-			local amount = nil
-			local inAmountStr = element:children():get(3):text()
-			local outAmountStr = element:children():get(4):text()
-			local amountStr
-			if string.len(inAmountStr) > 0 then
-				amountStr = inAmountStr
-			else
-				amountStr = "-" .. outAmountStr
-			end
-			amountStr = string.gsub(amountStr, ",", "")
-			amount = tonumber(amountStr)
+            local descriptionElement = element:children():get(2)
+            local description = descriptionElement:text()
+            
+            if (string.sub(description, 0, 2)) == 'CT' then
+                bookingText = 'Credit Transfer'
+                description = string.sub(description, 3, -1)
+            elseif (string.sub(description, 0, 2)) == 'DD' then
+                bookingText = 'Direct Debit'
+                description = string.sub(description, 3, -1)
+            elseif (string.sub(description, 0, 3)) == 'POS' then
+                bookingText = 'Debit Card Transaction'
+                description = string.sub(description, 4, -1)
+            elseif (string.sub(description, 0, 3)) == 'CNC' then
+                bookingText = 'Debit Card Transaction (Contactless)'
+                description = string.sub(description, 4, -1)
+            elseif (string.sub(description, 0, 3)) == 'ATM' then
+                bookingText = 'ATM Transaction'
+                description = string.sub(description, 4, -1)
+            end
+            
+            -- Get SEPA details
+            local sepaForm = descriptionElement:xpath(".//form")
+            if sepaForm:length() > 0 then
+                local sepaPage = HTML(connection:request(sepaForm:submit()))
+                if sepaPage == nil then
+                    error("Could not retrieve SEPA details for " .. firstElement:text() .. " " .. description)
+                end
+                   local sepaVars = sepaPage:xpath("//div[@class='value']")
+                description = sepaVars:get(1):text()
+                purpose = sepaVars:get(3):text() .. " " .. sepaVars:get(9):text()
+                endToEndReference = sepaVars:get(8):text()
+            end
+            
+            local amount = nil
+            local inAmountStr = element:children():get(3):text()
+            local outAmountStr = element:children():get(4):text()
+            local amountStr
+            if string.len(inAmountStr) > 0 then
+                amountStr = inAmountStr
+            else
+                amountStr = "-" .. outAmountStr
+            end
+            amountStr = string.gsub(amountStr, ",", "")
+            amount = tonumber(amountStr)
 
-			table.insert(transactions, {
-				name = description,
-				bookingDate = timestamp,
-				purpose = purpose,
-				amount = amount,
-				bookingText = bookingText,
-				endToEndReference = endToEndReference
-			})
+            table.insert(transactions, {
+                name = description,
+                bookingDate = timestamp,
+                purpose = purpose,
+                amount = amount,
+                bookingText = bookingText,
+                endToEndReference = endToEndReference
+            })
 
-		end
-	)
+        end
+    )
 
     return {balance = balance, transactions = transactions}
 end
 
 function EndSession ()
-	print(HTML(connection:request("GET", logoutUrl)):xpath("//h1"):text())
+    print(HTML(connection:request("GET", logoutUrl)):xpath("//h1"):text())
 end
 
 -- permanent tsb formatting helpers --------------------------------------------------------------------------------------
